caf8a9b7d86a31a3d6a99f04c4655602bd6b4b0d
5m <5m@DESKTOP-EHD8S3P>
2025-10-06 09:44:27 +0200

    Dodaj pliki projektów.

---
+++ czcionka/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace czcionka
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ czcionka/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace czcionka
+{
+    public partial class MainPage : ContentPage
+    {
+        // Indeks śledzący, który cytat jest obecnie wyświetlany
+        int quoteIndex = 0;
+
+        // Tablica dostępnych cytatów
+        readonly string[] quotes = { "Dzień dobry", "Good morning", "Buenos dias" };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Ustawienie początkowego rozmiaru i wartości etykiety przy starcie aplikacji
+            // (Zapewnienie działania funkcji suwaka dla stanu początkowego)
+            SizeValueLabel.Text = SizeSlider.Value.ToString("F0");
+            QuoteLabel.FontSize = SizeSlider.Value;
+        }
+
+       
+        private void OnSliderValueChanged(object? sender, ValueChangedEventArgs e)
+        {
+            
+            int integerValue = (int)Math.Round(e.NewValue);
+
+            
+            SizeValueLabel.Text = integerValue.ToString();
+
+            
+            QuoteLabel.FontSize = integerValue;
+        }
+
+        
+        private void OnChangeTextClicked(object? sender, EventArgs e)
+        {
+            
+            quoteIndex++;
+
+           
+            if (quoteIndex >= quotes.Length)
+            {
+                quoteIndex = 0;
+            }
+
+           
+            QuoteLabel.Text = quotes[quoteIndex];
+
+           
+            ChangeTextBtn.Text = "Zmień tekst";
+        }
+    }
+}
